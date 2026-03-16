df9234467ae9d0b7ce3b23c6c49246022a04a2a7
lorenzo-panza <lolopnz04@gmail.com>
2026-03-16 17:30:34 +0100

    Add uncertainty agent skeleton

--- processing_pipeline.py
+++ processing_pipeline.py
@@ -1,13 +1,18 @@
 import sqlite3
 from transformers import pipeline
+from uncertainty_agent import compute_uncertainty
 
 
 def run_processing_pipeline():
-    # 1) Charger FinBERT
+    
+    # 1) Chargement de Modele
+    # FinBERT
     sentiment_model = pipeline(
         "text-classification",
         model="ProsusAI/finbert"
     )
+    # Agent-Uncertainty
+    #.....
 
     # 2) Connexion à la base source
     conn = sqlite3.connect("news_database.db")
@@ -19,8 +24,9 @@ def run_processing_pipeline():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS article_scores (
             url TEXT PRIMARY KEY,
-            polarity REAL,
-            confidence REAL
+            polarity INTEGER,
+            polarity_conf REAL,
+            uncertainty REAL
         )
     """)
     conn.commit()
@@ -40,6 +46,7 @@ def run_processing_pipeline():
         # On coupe si le texte est trop long pour éviter les soucis
         text_to_analyze = text_to_analyze[:1500]
 
+        #-------------FinBert----------Polarity----------------
         result = sentiment_model(text_to_analyze)[0]
 
         label = result["label"]
@@ -47,27 +54,33 @@ def run_processing_pipeline():
 
         # Conversion label -> polarity
         if label == "positive":
-            polarity = score
+            polarity = 1
         elif label == "negative":
-            polarity = -score
+            polarity = -1
         else:
-            polarity = 0.0
+            polarity = 0
 
-        confidence = score
+        conf_pol = score
+        #------------------------------------------------------
 
+        #-------------Agent---------Uncertainty----------------
+        uncertainty = compute_uncertainty(text_to_analyze)
+        #------------------------------------------------------
+
+        #Insertion dans la table
         cursor.execute("""
-            INSERT OR REPLACE INTO article_scores (url, polarity, confidence)
-            VALUES (?, ?, ?)
-        """, (url, polarity, confidence))
+            INSERT OR REPLACE INTO article_scores (url, polarity, polarity_conf, uncertainty)
+            VALUES (?, ?, ?, ?)
+        """, (url, polarity, conf_pol, uncertainty))
 
         print("\n------------------------------")
         print(f"Article traité : {title}")
-        print(f"  label = {label}, polarity = {polarity:.3f}, confidence = {confidence:.3f}")
+        print(f"  label = {label}, polarity = {polarity}, confidence = {conf_pol:.3f}")
 
     conn.commit()
     conn.close()
 
-    print("Scores FinBERT générés et enregistrés.")
+    print("Scores générés et enregistrés.")
     print("Fin du processing.")
 
 
---
+++ uncertainty_agent.py
@@ -0,0 +1,2 @@
+def compute_uncertainty(text):
+    return 1.0
